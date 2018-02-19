1f988dea322f84eed18daf283c22d000756423da
Nils Petersson <nils.h.petersson@gmail.com>
2018-02-19 17:09:43 +0100

    feat(DamageText): Text now rises and has more stability

--- TDDD57/Assets/Scripts/DamageText.cs
+++ TDDD57/Assets/Scripts/DamageText.cs
@@ -7,19 +7,29 @@ public class DamageText : MonoBehaviour {
 
 	GameObject cam;
 	GameObject txt;
+	GameObject fighter;
 	int stability_counter = 0;
 	int active_timer = 0;
 	public Text txtRef;
+	Vector3 newPos;
+
 	// Use this for initialization
 	void Start () {
 		cam = GameObject.Find("ARCamera");
 		txtRef = GameObject.Find("Text").GetComponent<Text>();
+		fighter = GameObject.Find("littleswordfighter");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		StabilizeText();
+		FadeUp();
 		DeactivateText();
+	}
+
+	void FadeUp(){
+		newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		newPos[1] += 0.1f;
+		transform.position = newPos;
 	}
 
 	void DeactivateText(){
@@ -30,17 +40,19 @@ public class DamageText : MonoBehaviour {
 		}
 	}
 
-	void StabilizeText(){
+	/*void StabilizeText(){
 		stability_counter++;
 		if (stability_counter == 30){ //To avoid shaky text
 			transform.position = new Vector3(cam.transform.position.x/2, cam.transform.position.y/2, cam.transform.position.z/2);
 			transform.LookAt(cam.transform.position);
 			stability_counter = 0;
 		}
-	}
+	}*/
 
 	public void TextActive(int damage){
 		txtRef.text = "" + damage;
 		active_timer = 0;
+		transform.position = new Vector3(0, 0.5f, 0);
+		transform.LookAt(cam.transform.position, fighter.transform.up);
 	}
 }
