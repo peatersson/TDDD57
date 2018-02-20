ef60fca03de1a68c73c038342e671df2c2daa033
Nils Petersson <nils.h.petersson@gmail.com>
2018-02-20 12:30:16 +0100

    Feat(DamageText): Multiple texts can now spawn

--- TDDD57/Assets/Scripts/DamageText.cs
+++ TDDD57/Assets/Scripts/DamageText.cs
@@ -7,52 +7,21 @@ public class DamageText : MonoBehaviour {
 
 	GameObject cam;
 	GameObject txt;
-	GameObject fighter;
-	int stability_counter = 0;
-	int active_timer = 0;
-	public Text txtRef;
-	Vector3 newPos;
 
 	// Use this for initialization
 	void Start () {
-		cam = GameObject.Find("ARCamera");
-		txtRef = GameObject.Find("Text").GetComponent<Text>();
-		fighter = GameObject.Find("littleswordfighter");
+		txt = GameObject.Find("Text");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		FadeUp();
-		DeactivateText();
 	}
 
-	void FadeUp(){
-		newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-		newPos[1] += 0.1f;
-		transform.position = newPos;
-	}
-
-	void DeactivateText(){
-		active_timer++;
-		if (active_timer == 300){
-			txtRef.text = "";
-			active_timer = 0;
-		}
-	}
-
-	/*void StabilizeText(){
-		stability_counter++;
-		if (stability_counter == 30){ //To avoid shaky text
-			transform.position = new Vector3(cam.transform.position.x/2, cam.transform.position.y/2, cam.transform.position.z/2);
-			transform.LookAt(cam.transform.position);
-			stability_counter = 0;
-		}
-	}*/
 
 	public void TextActive(int damage){
-		txtRef.text = "" + damage;
-		active_timer = 0;
-		transform.position = new Vector3(0, 0.5f, 0);
-		transform.LookAt(cam.transform.position, fighter.transform.up);
+		var textChild = Instantiate (txt, transform);
+		textChild.transform.position = new Vector3(0, 0.5f, 0);
+		textChild.GetComponent<Text>().text = "" + damage;
+		Destroy(textChild, 2f);
 	}
 }
---
+++ TDDD57/Assets/Scripts/RisingText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingText : MonoBehaviour {
+
+	Vector3 newPos;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		newPos[1] += 0.1f;
+		transform.position = newPos;
+	}
+}
